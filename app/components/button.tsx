b79196749016c61024e1ff969ffd2938eb0c4d8a
neza <neza.djukic@gmail.com>
2024-11-16 21:28:18 +0100

    Ada-73: add base components for the website (#1)
    * Add icons package
    
    * Add a bunch of styles to tailwind
    
    * Add a util for typing components easier
    
    * Add base for button component
    
    * Add icons as a prop instead
    
    * Add variants and sizes
    
    * Fix color names
    
    * Add correct hover and disabled states
    
    * Fix it for secondary button too
    
    * Fix exports
    
    * Add heading component
    
    * Fix heading sizes
    
    * Add paragraph

---
+++ app/components/button.tsx
@@ -0,0 +1,91 @@
+import { ElementType, forwardRef } from "react";
+import { ForwardRefComponent, PropsOf } from "../utils/polymorphic";
+import { tv } from "tailwind-variants";
+
+const baseButton = tv({
+  base: "inline-flex shrink-0 relative justify-center items-center gap-1.5 select-none rounded-lg font-button font-medium text-center whitespace-nowrap outline-none",
+  variants: {
+    size: {
+      sm: "text-sm h-8 py-1 px-4",
+      md: "text-lg h-11 py-2 px-5",
+    },
+    variant: {
+      primary:
+        "border border-black text-white hover:text-black bg-red hover:shadow-button hover:mt-minus-4 hover:ml-1",
+      secondary:
+        "border border-black text-black bg-white hover:shadow-button hover:mt-minus-4 hover:ml-1",
+      disabled:
+        "cursor-not-allowed bg-gray300 border border-gray700 text-white",
+    },
+  },
+});
+
+interface ButtonOptions {
+  /**
+   * Sets the size of the button
+   * @default 'md'
+   */
+  size?: "sm" | "md";
+  /**
+   * Sets the style variant of the button
+   * @default 'secondary'
+   */
+  variant?: "primary" | "secondary";
+  /**
+   * If `true` the button will be disabled
+   * @default false
+   */
+  isDisabled?: boolean;
+  /**
+   * The HTML `type` attribute
+   * @default 'button'
+   */
+  type?: "button" | "submit" | "reset";
+  /**
+   * Icon to display after the button label
+   */
+  iconRight?: ElementType;
+  /**
+   * Icon to display before the button label
+   */
+  iconLeft?: ElementType;
+}
+
+type PolymorphicButton = ForwardRefComponent<"button", ButtonOptions>;
+export type ButtonProps = PropsOf<PolymorphicButton>;
+
+export const Button = forwardRef(
+  (
+    {
+      children,
+      type = "button",
+      size = "md",
+      variant = "secondary",
+      isDisabled = false,
+      disabled: hasHtmlDisabledProp,
+      iconRight: IconRight,
+      iconLeft: IconLeft,
+      ...rest
+    },
+    forwardedRef
+  ) => {
+    const shouldBeDisabled = isDisabled || hasHtmlDisabledProp;
+
+    return (
+      <button
+        ref={forwardedRef}
+        type={type}
+        disabled={shouldBeDisabled}
+        className={baseButton({
+          size,
+          variant: shouldBeDisabled ? "disabled" : variant,
+        })}
+        {...rest}
+      >
+        {IconLeft && <IconLeft className="w-4 h-4" />}
+        {children}
+        {IconRight && <IconRight className="w-4 h-4" />}
+      </button>
+    );
+  }
+) as PolymorphicButton;
---
+++ app/components/paragraph.tsx
@@ -0,0 +1,99 @@
+import { forwardRef } from "react";
+import { ForwardRefComponent, PropsOf } from "../utils/polymorphic";
+import { tv } from "tailwind-variants";
+
+const baseParagraph = tv({
+  base: "font-paragraph m-0 break-words",
+  variants: {
+    size: {
+      xs: "text-xs",
+      sm: "text-sm",
+      md: "text-base",
+      lg: "text-lg",
+      xl: "text-xl",
+    },
+    weight: {
+      regular: "font-normal",
+      medium: "font-medium",
+      semiBold: "font-semibold",
+      bold: "font-bold",
+      extraBold: "font-extrabold",
+    },
+    textAlign: {
+      left: "text-left",
+      center: "text-center",
+      right: "text-right",
+    },
+    color: {
+      black: "text-black",
+      white: "text-white",
+    },
+    truncate: {
+      true: "truncate",
+    },
+  },
+});
+
+interface ParagraphOptions {
+  /**
+   * Sets the visual size of the paragraph.
+   *
+   * @default 'md'
+   */
+  size?: "xl" | "lg" | "md" | "sm" | "xs";
+  /**
+   * Sets the font weight of the paragraph.
+   *
+   * @default 'md'
+   */
+  weight?: "regular" | "medium" | "semiBold" | "bold" | "extraBold";
+  /**
+   * Sets the color of the paragraph
+   * @default 'black'
+   */
+  color?: "black" | "white";
+  /**
+   * Truncates the paragraph if true
+   */
+  shouldTruncate?: boolean;
+  /**
+   * Alignment of the paragraph
+   * @default 'left'
+   */
+  textAlign?: "left" | "center" | "right";
+}
+
+type ParagraphComponent = ForwardRefComponent<"p", ParagraphOptions>;
+export type ParagraphProps = PropsOf<ParagraphComponent>;
+
+export const Paragraph = forwardRef(
+  (
+    {
+      as,
+      children,
+      size = "md",
+      color = "black",
+      textAlign = "left",
+      weight = "regular",
+      shouldTruncate,
+      ...rest
+    },
+    forwardedRef
+  ) => {
+    return (
+      <p
+        ref={forwardedRef}
+        className={baseParagraph({
+          size,
+          color,
+          weight,
+          textAlign,
+          truncate: shouldTruncate,
+        })}
+        {...rest}
+      >
+        {children}
+      </p>
+    );
+  }
+) as ParagraphComponent;
