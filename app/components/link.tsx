3ee44ae2aa52aa5b618eccc0e0f0064f05c6bb23
dj-neza <neza.djukic@gmail.com>
2024-11-16 22:59:25 +0100

    Add link functionality

--- app/components/link.tsx
+++ app/components/link.tsx
@@ -1,3 +1,36 @@
-export function Link() {
-  return null;
+import NextLink from "next/link";
+import { ForwardRefComponent, PropsOf } from "../utils/polymorphic";
+import { forwardRef } from "react";
+
+interface LinkOptions {
+  /**
+   * The URL to link to.
+   */
+  href: string;
+  /**
+   * If `true`, the link will open in new tab
+   */
+  isExternal?: boolean;
 }
+
+type LinkComponent = ForwardRefComponent<"a", LinkOptions>;
+export type LinkProps = PropsOf<LinkComponent>;
+
+export const Link = forwardRef(
+  ({ children, href, isExternal = false, ...restProps }, forwardedRef) => {
+    const externalProps = isExternal
+      ? { target: "_blank", rel: "noopener noreferrer" }
+      : {};
+
+    return (
+      <NextLink
+        ref={forwardedRef}
+        href={href}
+        {...externalProps}
+        {...restProps}
+      >
+        {children}
+      </NextLink>
+    );
+  }
+) as LinkComponent;
