f19ab4749a6a96d83fdad285497c038f49e7b0c2
dj-neza <neza.djukic@gmail.com>
2024-11-17 15:29:00 +0100

    Add a navbar menu component

---
+++ app/components/navbar-menu.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Button } from "./button";
+import { Link } from "./link";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
+import { useState } from "react";
+
+export function NavbarMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="block md:hidden">
+      <button
+        data-dropdown-toggle="dropdown"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? (
+          <XMarkIcon className="w-8 h-8" />
+        ) : (
+          <Bars3Icon className="w-8 h-8" />
+        )}
+      </button>
+      <div
+        id="dropdown"
+        className={`p-5 absolute z-10 t-2 r-1 translate-x-[-80%] ${
+          isOpen ? "flex" : "hidden"
+        } bg-white rounded-2xl border border-black flex-col gap-6`}
+      >
+        <ol className="flex flex-col gap-3">
+          <li>
+            <Link href="/o-nas">O nas</Link>
+          </li>
+          <li>
+            <Link href="/dogodki">Dogodki</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/partnerstvo">Partnerstva</Link>
+          </li>
+        </ol>
+        <Button>Pridruži se nam</Button>
+      </div>
+    </div>
+  );
+}
