56515fe11a7d9ab7a4bbd8d7ab5dbdb92c7c9928
dj-neza <neza.djukic@gmail.com>
2024-11-23 16:37:50 +0100

    Add base for hero to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,58 @@
+import Image from "next/image";
+import { PageWrapper } from "./components/page-wrapper";
+import { Card } from "./components/card";
+import { Heading } from "./components/heading";
+import { Paragraph } from "./components/paragraph";
+import { CtaLink } from "./components/cta-link";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Klub Ada</h1>
-    </div>
+    <PageWrapper>
+      <div className="flex flex-col gap-4 md:flex-row md:items-stretch">
+        <div className="flex flex-col gap-4 shrink-1">
+          <Card bgColor="bg-white">
+            <div className="flex flex-col gap-8">
+              <div className="hidden lg:block">
+                <Heading size="xl">{"Skupnost žensk v tehnologiji"}</Heading>
+              </div>
+              <div className="block lg:hidden">
+                <Heading size="md">{"Skupnost žensk v tehnologiji"}</Heading>
+              </div>
+              <Paragraph size="sm" weight="medium">
+                {
+                  "Klub Ada organizira dogodke, ki so namenjeni mreženju in deljenju znanja, kariernega in osebnega razvoja za dijakinje, študentke, zaposlene in vse, ki jih področje tehnologije zanima."
+                }
+              </Paragraph>
+            </div>
+          </Card>
+          <div className="block md:hidden grow">
+            <Image
+              src="/assets/hero.jpg"
+              width={768}
+              height={150}
+              alt="Hero picture"
+              className="rounded-2xl max-h-80 object-cover"
+            />
+          </div>
+          <div className="flex flex-row gap-4">
+            <div className="grow shrink-0">
+              <CtaLink label="Pridi na dogodek" color="red" href="/" />
+            </div>
+            <div className="grow shrink-0">
+              <CtaLink label="Postani partner" color="blue" href="/" />
+            </div>
+          </div>
+        </div>
+        <div className="hidden md:block grow">
+          <Image
+            src="/assets/hero.jpg"
+            width={900}
+            height={600}
+            alt="Hero picture"
+            className="rounded-2xl w-full h-full object-cover"
+          />
+        </div>
+      </div>
+    </PageWrapper>
   );
 }
