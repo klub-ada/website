7467fdb75db878b2055c492a981b7f7c7540678f
Kaja Skerlj <kaja.skerlj@gmail.com>
2024-11-26 16:28:55 +0100

    sm and xs heading

--- app/components/heading.tsx
+++ app/components/heading.tsx
@@ -6,9 +6,11 @@ const baseHeading = tv({
   base: "font-heading m-0 break-words",
   variants: {
     size: {
-      xl: "text-[36px] md:text-[56px] leading-[120%] font-bold",
+      xl: "text-[36px] md:text-[52px] leading-[120%] font-bold",
       lg: "text-3xl md:text-5xl leading-[120%] font-bold",
       md: "text-text-2xl md:text-4xl leading-[120%] font-bold",
+      sm: "text-2xl md:text-[32px] leading-[120%] font-bold",
+      sx: "text-xl md:text-2xl leading-[120%] font-bold",
     },
     textAlign: {
       left: "text-left",
@@ -31,7 +33,7 @@ interface HeadingOptions {
    *
    * @default 'lg'
    */
-  size?: "xl" | "lg" | "md";
+  size?: "xl" | "lg" | "md" | "sm" | "xs";
   /**
    * Sets the color of the heading
    * @default 'black'
