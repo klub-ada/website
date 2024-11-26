954f71dbe71e684f785b04bcb271853b24f3d998
Kaja Skerlj <kaja.skerlj@gmail.com>
2024-11-26 16:27:55 +0100

    Card component copy

---
+++ app/components/card.tsx
@@ -0,0 +1,20 @@
+type CardProps = {
+    children: string | JSX.Element | JSX.Element[];
+    bgColor?:
+      | "bg-white"
+      | "bg-beige"
+      | "bg-red"
+      | "bg-blue"
+      | "bg-pink"
+      | "bg-red100"
+      | "bg-blue100";
+  };
+  export function Card({ children, bgColor = "bg-beige" }: CardProps) {
+    return (
+        <article
+          className={`p-6 md:p-8 ${bgColor} rounded-2xl border border-black`}
+        >
+          {children}
+        </article>
+      );
+    }
