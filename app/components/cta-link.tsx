99ea472c9d3364acdec12959f0cedb21e9a8e343
dj-neza <neza.djukic@gmail.com>
2024-11-27 20:26:27 +0100

    Add extra styling to cta-link

--- app/components/cta-link.tsx
+++ app/components/cta-link.tsx
@@ -6,7 +6,7 @@ import { ArrowRightIcon } from "@heroicons/react/16/solid";
 import NextLink from "next/link";
 
 const baseCtaLink = tv({
-  base: "flex flex-row grow p-6 gap-6 min-h-40 select-none rounded-2xl font-button font-medium outline-none border border-black hover:shadow-button top-0 left-0 hover:top-[-4px] hover:left-1 text-start",
+  base: "flex flex-col grow p-6 gap-6 min-h-40 select-none rounded-2xl font-button font-medium outline-none border border-black hover:shadow-button top-0 left-0 hover:top-[-4px] hover:left-1 text-start",
   variants: {
     color: {
       red: "bg-red",
@@ -55,12 +55,12 @@ export const CtaLink = forwardRef(
         className={baseCtaLink({ color })}
         {...rest}
       >
-        <div className="max-w-[50%]">
-          <Paragraph size="xl" weight="bold">
+        <div className="max-w-[80%] md:max-w-[50%]">
+          <Paragraph size="xl" weight="bold" className="line-clamp-2">
             {label}
           </Paragraph>
         </div>
-        <div className="grow self-end">
+        <div className="grow justify-self-end">
           <div className="justify-self-end border border-black rounded-lg p-3">
             <ArrowRightIcon className="w-4 h-4 text-black" />
           </div>
