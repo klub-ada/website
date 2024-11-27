61facb4d7e9ca10fc19b6c38be91de109dfa4370
Kaja Skerlj <kaja.skerlj@gmail.com>
2024-11-27 21:19:38 +0100

    3 small changes based on comments

--- app/page.tsx
+++ app/page.tsx
@@ -6,45 +6,50 @@ import Image from "next/image";
 import { Button } from "./components/button";
 export default function Page() {
   return (
+    <>
     <PageWrapper bgColor="bg-red">
-      <div className="flex flex-col py-20">
-        <div className="flex flex-col gap-16">
-          <div className="flex flex-col gap-8">
-            <div className="max-w-xl">
-              <Heading size="lg">{"Pridi na naš naslednji dogodek!"}</Heading>
+      <div className="flex flex-col py-20 gap-16">
+        <div className="flex flex-col gap-8">
+          <div className="max-w-xl">
+            <Heading size="lg">{"Pridi na naš naslednji dogodek!"}</Heading>
+          </div>
+          <div className="">
+            <Button size="md" variant="secondary">Vsi dogodki</Button>
+          </div>
+        </div>
+        <Card bgColor="bg-red100">
+          <div className="flex flex-row gap-8 justify-start">
+            <div className="block">
+              <Image
+                src="/assets/dogodki.jpg"
+                width={500}
+                height={500}
+                alt="Naslednji dogodek"
+                className="rounded-2xl object-fill"
+              />
             </div>
-            <div className="">
-              <Button size="md" variant="secondary">Vsi dogodki</Button>
+            <div className="flex flex-col gap-12 justify-center max-w-xl">
+              <div className="flex flex-col gap-3">
+                <Paragraph size="lg">{"23. 10. 2024 ob 17:00 @ FRI v P04"}</Paragraph>
+                <Heading size="sm">{"Kako se pripravim na tehnični intervju?"}</Heading>
+                <Paragraph size="lg">
+                  {"Nasveti za pripravo na tehnični intervju in oblikovanje CV-ja ter primeri dobrih praks v obliki simulacije tehničnega intervjuja."
+                  }
+                </Paragraph>
+              </div>
+              <div className="">
+                <Button size="md">Pridruži se</Button>
+              </div>
             </div>
           </div>
-          <Card bgColor="bg-white">
-            <div className="flex flex-rox gap-8 justify-start">
-              <div className="block">
-                <Image
-                  src="/assets/dogodki.jpg"
-                  width={500}
-                  height={500}
-                  alt="Naslednji dogodek"
-                  className="rounded-2xl object-fill"
-                />
-              </div>
-              <div className="flex flex-col gap-12 justify-center max-w-xl">
-                <div className="flex flex-col gap-3">
-                  <Paragraph size="lg">{"23. 10. 2024 ob 17:00 @ FRI v P04"}</Paragraph>
-                  <Heading size="sm">{"Kako se pripravim na tehnični intervju?"}</Heading>
-                  <Paragraph size="lg">
-                    {"Nasveti za pripravo na tehnični intervju in oblikovanje CV-ja ter primeri dobrih praks v obliki simulacije tehničnega intervjuja."
-                    }
-                  </Paragraph>
-                </div>
-                <div className="">
-                  <Button size="md">Pridruži se</Button>
-                </div>
-              </div>
-            </div>
-          </Card>
-        </div>
+        </Card>
       </div>
     </PageWrapper>
+    <PageWrapper>
+      <div className="">
+
+      </div>
+    </PageWrapper>
+    </>
   );
 }
