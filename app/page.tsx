ca9297c8c017f00d55b6610510b28fc1a33c6380
Kaja Skerlj <kaja.skerlj@gmail.com>
2024-11-27 22:43:37 +0100

    Added new segment but still figuring out the right proportions for column 1 and 2

--- app/page.tsx
+++ app/page.tsx
@@ -45,9 +45,123 @@ export default function Page() {
         </Card>
       </div>
     </PageWrapper>
-    <PageWrapper>
-      <div className="">
 
+    <PageWrapper>
+      <div className="flex flex-col py-20 gap-6">
+        <div className="flex flex-row gap-6">
+          <div className="flex flex-col gap-4 max-w-xs">
+            <Heading size="lg">{"Aktivnosti"}</Heading>
+            <Paragraph size="lg">
+              {"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
+            </Paragraph>
+          </div>
+            <Card bgColor="bg-white">
+              <div className="flex flex-col gap-9">
+                <div className="flex flex-col max-w-sm gap-4">
+                  <Heading size="sm">{"Mesečni dogodki"}</Heading>
+                  <Paragraph size="lg">
+                    {"Na mesečnih srečanjih obravnavamo različne karierne in tehnične teme, govorimo o mehkih veščinah, gostimo strokovnjakinje iz industrije ter se družimo in izmenjujemo ideje v sproščenem okolju ob prigrizkih in pijači."
+                    }
+                  </Paragraph>
+                </div>
+                <div className="">
+                  <Image
+                    src="/assets/dogodki.jpg"
+                    width={400}
+                    height={400}
+                    alt="Mesecni dogodeki"
+                    className="rounded-2xl object-fill"
+                  />
+                </div>
+              </div>
+            </Card>
+
+            <Card bgColor="bg-white">
+              <div className="flex flex-col gap-9">
+                <div className="flex flex-col max-w-sm gap-4">
+                  <Heading size="sm">{"Coffee&&Code"}</Heading>
+                  <Paragraph size="lg">
+                    {"Serija neformalnih druženj ob kavi, kjer skupaj programiramo na projektih, ki si jih vsaka izbere sama. Lahko rešuješ domačo nalogo, delaš na projektu, slediš tutorialu ali poiščeš motivacijo za začetek novega projekta."
+                    }
+                  </Paragraph>
+                </div>
+                <div className="">
+                  <Image
+                    src="/assets/coffee&&code.png"
+                    width={400}
+                    height={400}
+                    alt="Coffee&&Code druzenje"
+                    className="rounded-2xl object-fill"
+                  />
+                </div>
+              </div>
+            </Card>
+        </div>
+
+        <div className="flex flex-row gap-4">
+          <Card bgColor="bg-white">
+            <div className="flex flex-col gap-9">
+              <div className="flex flex-col max-w-sm gap-4">
+                <Heading size="sm">{"Discord skupnost"}</Heading>
+                <Paragraph size="lg">
+                  {"Skupnost je varen prostor za komunikacijo, kjer izmenjujemo ideje, razpravljamo o zanimivih temah, delimo dogodke, možnosti za zaposlitev, koristne izobraževalne vire in priložnosti za osebni ter karierni razvoj."
+                  }
+                </Paragraph>
+              </div>
+              <div className="">
+                <Image
+                  src="/assets/discord.png"
+                  width={400}
+                  height={400}
+                  alt="Discord skupina"
+                  className="rounded-2xl object-fill"
+                />
+              </div>
+            </div>
+          </Card>
+
+          <Card bgColor="bg-white">
+            <div className="flex flex-col gap-9">
+              <div className="flex flex-col max-w-sm gap-4">
+                <Heading size="sm">{"Knjižni klub"}</Heading>
+                <Paragraph size="lg">
+                  {"Srečanje za vse, ki poleg tehnologije uživamo tudi v branju. Vsakih nekaj mesecev v Discord skupini izberemo knjigo, vezano na teme, kot so tehnologija, vloga žensk v družbi, osebnostna rast in razvoj kariere."
+                  }
+                </Paragraph>
+              </div>
+              <div className="">
+                <Image
+                  src="/assets/knjizni-klub.png"
+                  width={400}
+                  height={400}
+                  alt="Knjizni klub"
+                  className="rounded-2xl object-fill"
+                />
+              </div>
+            </div>
+          </Card>
+
+          <Card bgColor="bg-white">
+            <div className="flex flex-col gap-9">
+              <div className="flex flex-col max-w-sm gap-4">
+                <Heading size="sm">{"Novičnik"}</Heading>
+                <Paragraph size="lg">
+                  {"Preko novičnika delimo vabila na prihajajoče dogodke in povzetke dogodkov, uporabne nasvete, povezave do drugih koristnih virov, kot so hackathoni, karierni dogodki, zaposlitvene priložnosti in še več."
+                  }
+                </Paragraph>
+              </div>
+              <div className="">
+                <Image
+                  src="/assets/novicnik.png"
+                  width={400}
+                  height={400}
+                  alt="Novicnik"
+                  className="rounded-2xl object-fill"
+                />
+              </div>
+            </div>
+          </Card>
+        </div>
       </div>
     </PageWrapper>
     </>
