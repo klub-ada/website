4ef57b44c1331e3a40cdb94087d9a2905d9e24e3
Kaja Skerlj <kaja.skerlj@gmail.com>
2024-11-28 18:45:23 +0100

    Made small changes for responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Page() {
   return (
     <>
     <PageWrapper bgColor="bg-red">
-      <div className="flex flex-col py-20 gap-16">
+      <div className="flex flex-col py-10 md:py-20 gap-8 md:gap-16">
         <div className="flex flex-col gap-8">
-          <div className="max-w-xl">
+          <div className="max-w-sm md:max-w-xl">
             <Heading size="lg">{"Pridi na naš naslednji dogodek!"}</Heading>
           </div>
           <div className="">
@@ -19,17 +19,17 @@ export default function Page() {
           </div>
         </div>
         <Card bgColor="bg-red100">
-          <div className="flex flex-row gap-8 justify-start">
+          <div className="md:flex gap-8">
             <div className="block">
               <Image
                 src="/assets/dogodki.jpg"
                 width={500}
                 height={500}
                 alt="Naslednji dogodek"
-                className="rounded-2xl object-fill"
+                className="h-80 w-full object-cover md:h-full md:w-full rounded-2xl"
               />
             </div>
-            <div className="flex flex-col gap-12 justify-center max-w-xl">
+            <div className="flex flex-col pt-4 gap-6 md:gap-12 justify-center max-w-xl flex-grow">
               <div className="flex flex-col gap-3">
                 <Paragraph size="lg" weight="regular">{"23. 10. 2024 ob 17:00 @ FRI v P04"}</Paragraph>
                 <Heading size="sm">{"Kako se pripravim na tehnični intervju?"}</Heading>
@@ -48,15 +48,10 @@ export default function Page() {
     </PageWrapper>
 
     <PageWrapper>
-      <div className="flex flex-col py-20 gap-4">
+      <div className="flex flex-col py-10 md:py-20 gap-4">
         <div className="flex flex-col md:flex-row gap-4">
           <div className="flex flex-col gap-4 basis-1/3">
-            <div className="hidden xl:block">
-              <Heading size="lg">{"Aktivnosti"}</Heading>
-            </div>
-            <div className="block xl:hidden">
-              <Heading size="md">{"Aktivnosti"}</Heading>
-            </div>
+            <Heading size="lg">{"Aktivnosti"}</Heading>
             <Paragraph size="lg">
               {"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
             </Paragraph>
@@ -67,7 +62,6 @@ export default function Page() {
               description="Na mesečnih srečanjih obravnavamo različne karierne in tehnične teme, govorimo o mehkih veščinah, gostimo strokovnjakinje iz industrije ter se družimo in izmenjujemo ideje v sproščenem okolju ob prigrizkih in pijači."
               imageSrc="/assets/dogodki.jpg"
               imageAlt="Mesečni dogodki"
-              className=""
             />    
           </div>
           <div className="basis-1/3">
@@ -76,7 +70,6 @@ export default function Page() {
               description="Serija neformalnih druženj ob kavi, kjer skupaj programiramo na projektih, ki si jih vsaka izbere sama. Lahko rešuješ domačo nalogo, delaš na projektu, slediš tutorialu ali poiščeš motivacijo za začetek novega projekta."
               imageSrc="/assets/coffee&&code.png"
               imageAlt="Coffee&&Code"
-              className=""
             />    
           </div>
         </div>
@@ -88,7 +81,6 @@ export default function Page() {
               description="Skupnost je varen prostor za komunikacijo, kjer izmenjujemo ideje, razpravljamo o zanimivih temah, delimo dogodke, možnosti za zaposlitev, koristne izobraževalne vire in priložnosti za osebni ter karierni razvoj."
               imageSrc="/assets/discord.png"
               imageAlt="Discord skupnost"
-              className=""
             />    
           </div>
           <div className="basis-1/3">
@@ -97,7 +89,6 @@ export default function Page() {
               description="Srečanje za vse, ki poleg tehnologije uživamo tudi v branju in razpravah. Vsakih nekaj mesecev v Discord skupini izberemo knjigo, vezano na teme, kot so tehnologija, vloga žensk v družbi, osebnostna rast in razvoj kariere."
               imageSrc="/assets/knjizni-klub.png"
               imageAlt="Knjižni klub"
-              className=""
             />    
           </div>
           <div className="basis-1/3">
@@ -106,7 +97,6 @@ export default function Page() {
               description="Preko novičnika delimo vabila na prihajajoče dogodke in povzetke dogodkov, uporabne nasvete, povezave do drugih koristnih virov, kot so hackathoni, karierni dogodki, zaposlitvene priložnosti in še več."
               imageSrc="/assets/novicnik.png"
               imageAlt="Novičnik"
-              className=""
             />    
           </div>
         </div>
