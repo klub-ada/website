cbd32e2923f68dc0e55916b76cf305a33b55c6a4
Kaja Skerlj <kaja.skerlj@gmail.com>
2024-11-28 13:26:06 +0100

    Addes the component for activity cards and made adjustments for mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { Heading } from "./components/heading";
 import { Paragraph } from "./components/paragraph";
 import Image from "next/image";
 import { Button } from "./components/button";
+import { ActivityCard } from "./components/activity-card";
 export default function Page() {
   return (
     <>
@@ -30,9 +31,9 @@ export default function Page() {
             </div>
             <div className="flex flex-col gap-12 justify-center max-w-xl">
               <div className="flex flex-col gap-3">
-                <Paragraph size="lg">{"23. 10. 2024 ob 17:00 @ FRI v P04"}</Paragraph>
+                <Paragraph size="lg" weight="regular">{"23. 10. 2024 ob 17:00 @ FRI v P04"}</Paragraph>
                 <Heading size="sm">{"Kako se pripravim na tehnični intervju?"}</Heading>
-                <Paragraph size="lg">
+                <Paragraph size="lg" weight="regular">
                   {"Nasveti za pripravo na tehnični intervju in oblikovanje CV-ja ter primeri dobrih praks v obliki simulacije tehničnega intervjuja."
                   }
                 </Paragraph>
@@ -47,120 +48,67 @@ export default function Page() {
     </PageWrapper>
 
     <PageWrapper>
-      <div className="flex flex-col py-20 gap-6">
-        <div className="flex flex-row gap-6">
-          <div className="flex flex-col gap-4 max-w-xs">
-            <Heading size="lg">{"Aktivnosti"}</Heading>
+      <div className="flex flex-col py-20 gap-4">
+        <div className="flex flex-col md:flex-row gap-4">
+          <div className="flex flex-col gap-4 basis-1/3">
+            <div className="hidden xl:block">
+              <Heading size="lg">{"Aktivnosti"}</Heading>
+            </div>
+            <div className="block xl:hidden">
+              <Heading size="md">{"Aktivnosti"}</Heading>
+            </div>
             <Paragraph size="lg">
               {"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
             </Paragraph>
           </div>
-            <Card bgColor="bg-white">
-              <div className="flex flex-col gap-9">
-                <div className="flex flex-col max-w-sm gap-4">
-                  <Heading size="sm">{"Mesečni dogodki"}</Heading>
-                  <Paragraph size="lg">
-                    {"Na mesečnih srečanjih obravnavamo različne karierne in tehnične teme, govorimo o mehkih veščinah, gostimo strokovnjakinje iz industrije ter se družimo in izmenjujemo ideje v sproščenem okolju ob prigrizkih in pijači."
-                    }
-                  </Paragraph>
-                </div>
-                <div className="">
-                  <Image
-                    src="/assets/dogodki.jpg"
-                    width={400}
-                    height={400}
-                    alt="Mesecni dogodeki"
-                    className="rounded-2xl object-fill"
-                  />
-                </div>
-              </div>
-            </Card>
-
-            <Card bgColor="bg-white">
-              <div className="flex flex-col gap-9">
-                <div className="flex flex-col max-w-sm gap-4">
-                  <Heading size="sm">{"Coffee&&Code"}</Heading>
-                  <Paragraph size="lg">
-                    {"Serija neformalnih druženj ob kavi, kjer skupaj programiramo na projektih, ki si jih vsaka izbere sama. Lahko rešuješ domačo nalogo, delaš na projektu, slediš tutorialu ali poiščeš motivacijo za začetek novega projekta."
-                    }
-                  </Paragraph>
-                </div>
-                <div className="">
-                  <Image
-                    src="/assets/coffee&&code.png"
-                    width={400}
-                    height={400}
-                    alt="Coffee&&Code druzenje"
-                    className="rounded-2xl object-fill"
-                  />
-                </div>
-              </div>
-            </Card>
+          <div className="basis-1/3">
+            <ActivityCard
+              title="Mesečni dogodki"
+              description="Na mesečnih srečanjih obravnavamo različne karierne in tehnične teme, govorimo o mehkih veščinah, gostimo strokovnjakinje iz industrije ter se družimo in izmenjujemo ideje v sproščenem okolju ob prigrizkih in pijači."
+              imageSrc="/assets/dogodki.jpg"
+              imageAlt="Mesečni dogodki"
+              className=""
+            />    
+          </div>
+          <div className="basis-1/3">
+            <ActivityCard
+              title="Coffee&&Code"
+              description="Serija neformalnih druženj ob kavi, kjer skupaj programiramo na projektih, ki si jih vsaka izbere sama. Lahko rešuješ domačo nalogo, delaš na projektu, slediš tutorialu ali poiščeš motivacijo za začetek novega projekta."
+              imageSrc="/assets/coffee&&code.png"
+              imageAlt="Coffee&&Code"
+              className=""
+            />    
+          </div>
         </div>
 
-        <div className="flex flex-row gap-4">
-          <Card bgColor="bg-white">
-            <div className="flex flex-col gap-9">
-              <div className="flex flex-col max-w-sm gap-4">
-                <Heading size="sm">{"Discord skupnost"}</Heading>
-                <Paragraph size="lg">
-                  {"Skupnost je varen prostor za komunikacijo, kjer izmenjujemo ideje, razpravljamo o zanimivih temah, delimo dogodke, možnosti za zaposlitev, koristne izobraževalne vire in priložnosti za osebni ter karierni razvoj."
-                  }
-                </Paragraph>
-              </div>
-              <div className="">
-                <Image
-                  src="/assets/discord.png"
-                  width={400}
-                  height={400}
-                  alt="Discord skupina"
-                  className="rounded-2xl object-fill"
-                />
-              </div>
-            </div>
-          </Card>
-
-          <Card bgColor="bg-white">
-            <div className="flex flex-col gap-9">
-              <div className="flex flex-col max-w-sm gap-4">
-                <Heading size="sm">{"Knjižni klub"}</Heading>
-                <Paragraph size="lg">
-                  {"Srečanje za vse, ki poleg tehnologije uživamo tudi v branju. Vsakih nekaj mesecev v Discord skupini izberemo knjigo, vezano na teme, kot so tehnologija, vloga žensk v družbi, osebnostna rast in razvoj kariere."
-                  }
-                </Paragraph>
-              </div>
-              <div className="">
-                <Image
-                  src="/assets/knjizni-klub.png"
-                  width={400}
-                  height={400}
-                  alt="Knjizni klub"
-                  className="rounded-2xl object-fill"
-                />
-              </div>
-            </div>
-          </Card>
-
-          <Card bgColor="bg-white">
-            <div className="flex flex-col gap-9">
-              <div className="flex flex-col max-w-sm gap-4">
-                <Heading size="sm">{"Novičnik"}</Heading>
-                <Paragraph size="lg">
-                  {"Preko novičnika delimo vabila na prihajajoče dogodke in povzetke dogodkov, uporabne nasvete, povezave do drugih koristnih virov, kot so hackathoni, karierni dogodki, zaposlitvene priložnosti in še več."
-                  }
-                </Paragraph>
-              </div>
-              <div className="">
-                <Image
-                  src="/assets/novicnik.png"
-                  width={400}
-                  height={400}
-                  alt="Novicnik"
-                  className="rounded-2xl object-fill"
-                />
-              </div>
-            </div>
-          </Card>
+        <div className="flex flex-col md:flex-row gap-4">
+          <div className="basis-1/3">
+            <ActivityCard
+              title="Discord skupnost"
+              description="Skupnost je varen prostor za komunikacijo, kjer izmenjujemo ideje, razpravljamo o zanimivih temah, delimo dogodke, možnosti za zaposlitev, koristne izobraževalne vire in priložnosti za osebni ter karierni razvoj."
+              imageSrc="/assets/discord.png"
+              imageAlt="Discord skupnost"
+              className=""
+            />    
+          </div>
+          <div className="basis-1/3">
+            <ActivityCard
+              title="Knjižni klub"
+              description="Srečanje za vse, ki poleg tehnologije uživamo tudi v branju in razpravah. Vsakih nekaj mesecev v Discord skupini izberemo knjigo, vezano na teme, kot so tehnologija, vloga žensk v družbi, osebnostna rast in razvoj kariere."
+              imageSrc="/assets/knjizni-klub.png"
+              imageAlt="Knjižni klub"
+              className=""
+            />    
+          </div>
+          <div className="basis-1/3">
+            <ActivityCard
+              title="Novičnik"
+              description="Preko novičnika delimo vabila na prihajajoče dogodke in povzetke dogodkov, uporabne nasvete, povezave do drugih koristnih virov, kot so hackathoni, karierni dogodki, zaposlitvene priložnosti in še več."
+              imageSrc="/assets/novicnik.png"
+              imageAlt="Novičnik"
+              className=""
+            />    
+          </div>
         </div>
       </div>
     </PageWrapper>
