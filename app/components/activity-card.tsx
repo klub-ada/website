a188cb58aee834b5fbd7bf314fc39f4ff54fc3eb
Kaja Skerlj <kaja.skerlj@gmail.com>
2024-11-28 13:25:38 +0100

    Made a new component for the activity cards

---
+++ app/components/activity-card.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { Heading } from "./heading";
+import { Paragraph } from "./paragraph";
+
+type ActivityCardProps = {
+    title: string;
+    description: string;
+    imageSrc: string;
+    imageAlt: string;
+    className: string;
+}
+
+export function ActivityCard({ title, description, imageSrc, imageAlt, className = "" }: ActivityCardProps) {
+    return (
+        <article
+        className={'flex flex-col gap-6 bg-white rounded-2xl border border-black p-6 xl:p-8 ${className}'}
+        >
+            <div className="flex flex-col gap-4">
+                <div className="hidden lg:block">
+                    <Heading size="sm">{title}</Heading>
+                </div>
+                <div className="block lg:hidden">
+                    <Heading size="xs">{title}</Heading>
+                </div>
+                <div className="hidden lg:block">
+                    <Paragraph size="lg" weight="regular">{description}</Paragraph>
+                </div>
+                <div className="block lg:hidden">
+                    <Paragraph size="md" weight="regular">{description}</Paragraph>
+                </div>
+            </div>
+            <Image
+                src={imageSrc}
+                width={400}
+                height={400}
+                alt={imageAlt}
+                className="rounded-2xl w-full h-full object-cover"
+            />
+        </article>
+    )
+}
+  
