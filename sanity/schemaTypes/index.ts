9d2207ef1324a6922d619cc94d1de686130cda8e
dj-neza <neza.djukic@gmail.com>
2024-12-03 23:59:08 +0100

    Update activity type

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -4,7 +4,14 @@ import { blockContentType } from "./blockContentType";
 import { categoryType } from "./categoryType";
 import { postType } from "./postType";
 import { teamMemberType } from "./teamMemberType";
+import { activityType } from "./activityType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, teamMemberType],
+  types: [
+    blockContentType,
+    categoryType,
+    postType,
+    activityType,
+    teamMemberType,
+  ],
 };
--- sanity/structure.ts
+++ sanity/structure.ts
@@ -3,15 +3,19 @@ import type { StructureResolver } from "sanity/structure";
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title("Blog")
+    .title("Collections")
     .items([
       S.documentTypeListItem("post").title("Posts"),
       S.documentTypeListItem("category").title("Categories"),
+      S.divider(),
       S.documentTypeListItem("teamMember").title("Team Members"),
       S.divider(),
+      S.documentTypeListItem("activity").title("Activity"),
       ...S.documentTypeListItems().filter(
         (item) =>
           item.getId() &&
-          !["post", "category", "teamMember"].includes(item.getId()!)
+          !["post", "category", "teamMember", "activity"].includes(
+            item.getId()!
+          )
       ),
     ]);
