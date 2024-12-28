07693cf77edb455e909c7315677aff71728c7ea5
Kaja Skerlj <kaja.skerlj@gmail.com>
2024-12-28 14:27:39 +0100

    Adjusted the Card aligments

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -10,17 +10,23 @@ type CardProps = {
     | "bg-blue100"
     | "bg-blue200";
   hasBorder?: boolean;
+  contentAlignment?: "center" | "left"; // New prop for alignment
 };
 export function Card({
   children,
   bgColor = "bg-beige",
   hasBorder = true,
+  contentAlignment = "left", // Default alignment
 }: CardProps) {
+  const alignmentClass =
+    contentAlignment === "center"
+      ? "items-center justify-center text-center"
+      : "items-start text-left";
   return (
     <article
       className={`p-6 md:p-8 ${bgColor} grow rounded-2xl ${
         hasBorder ? "border border-black" : ""
-      }`}
+      } flex flex-col ${alignmentClass}`}
     >
       {children}
     </article>
