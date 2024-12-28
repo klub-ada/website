2acd9fcf2cf103ebc70bccd85c75b8b906c92071
Kaja Skerlj <kaja.skerlj@gmail.com>
2024-12-28 14:28:41 +0100

    Added the Page for partnerships

--- app/(website)/partnerstvo/page.tsx
+++ app/(website)/partnerstvo/page.tsx
@@ -1,6 +1,10 @@
+import { Footer } from "@/app/components/footer";
 import { Collaboration } from "./collaboration";
 import { Hero } from "./hero";
+import { Statistics } from "./statistics";
 import { WhyPartner } from "./why-partner";
+import { PartnersList } from "./partners-list";
+import { BecomePartner } from "./become-partner";
 
 export default function Page() {
   return (
@@ -8,6 +12,10 @@ export default function Page() {
       <Hero />
       <WhyPartner />
       <Collaboration />
+      <Statistics />
+      <BecomePartner />
+      <PartnersList />
+      <Footer />
     </>
   );
 }
