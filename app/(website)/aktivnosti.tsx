140c44d77a3c313125e6d904381563eaaad0c6a4
Kaja Skerlj <kaja.skerlj@gmail.com>
2025-01-14 14:31:14 +0100

    Added a button

--- app/(website)/aktivnosti.tsx
+++ app/(website)/aktivnosti.tsx
@@ -5,6 +5,7 @@ import { PageWrapper } from "../components/page-wrapper";
 import { Paragraph } from "../components/paragraph";
 import { SanityDocument } from "next-sanity";
 import imageLoader from "../utils/image-loader";
+import { LinkButton } from "../components/link-button";
 
 const ACTIVITIES_QUERY = `*[
   _type == "activity"] | order(_updatedAt desc)`;
@@ -21,6 +22,17 @@ export async function Aktivnosti() {
             <Paragraph size="lg">
               {"Preberi si vse aktivnosti, ki jih ponujamo."}
             </Paragraph>
+            <div>
+              <LinkButton
+                size="md"
+                variant="secondary"
+                href="https://linktr.ee/klubada"
+                isExternal
+                showIcon
+              >
+                Koledar aktivnosti
+              </LinkButton>
+            </div>
           </div>
           {activities.map(({ name, description, activityImage }) => {
             const imageSrc = imageLoader(activityImage);
